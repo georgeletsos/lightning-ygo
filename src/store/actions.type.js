cc6f1b2ed3eb2657b1c74ee3b0bf22ec40ad231a
George Letsos <it1017@uom.edu.gr>
2020-04-26 21:27:05 +0300

    Fetch Prev & Next Display Cards

--- src/store/actions.type.js
+++ src/store/actions.type.js
@@ -1,5 +1,7 @@
 export const FETCH_CATALOG_CARDS = "fetchCatalogCards";
 export const FETCH_DISPLAY_CARDS = "fetchDisplayCards";
+export const FETCH_PREV_DISPLAY_CARD = "fetchPrevDisplayCard";
+export const FETCH_NEXT_DISPLAY_CARD = "fetchNextDisplayCard";
 export const RESET_CATALOG_CARDS = "resetCatalogCards";
 export const RESET_DISPLAY_CARDS = "resetDisplayCards";
 export const CHANGE_DISPLAY_CARD = "changeDisplayCard";
