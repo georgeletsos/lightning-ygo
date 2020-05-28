afeebcd14daaf74d2413b69a05b636677252962e
George Letsos <it1017@uom.edu.gr>
2020-05-28 21:10:10 +0300

    Added a file with mock data for testing

---
+++ src/tests/unit/mocks.js
@@ -0,0 +1,404 @@
+export const mockCards = [
+  {
+    cardType: "monster",
+    name: "Armored Cybern",
+    attribute: "wind",
+    level: 4,
+    monsterType: "machine",
+    types: ["union", "effect"],
+    text:
+      'Once per turn, you can either: Target 1 "Cyber Dragon" you control, or 1 Fusion Monster you control that lists "Cyber Dragon" as Fusion Material; equip this card to that target, OR: Unequip this card and Special Summon it. Once per turn, while equipped to a monster by this effect: You can target 1 face-up monster on the field; the equipped monster loses exactly 1000 ATK (even if this card leaves the field), and if it does, destroy that target. If the equipped monster would be destroyed by battle or card effect, destroy this card instead.',
+    atk: 0,
+    def: 2000,
+    image: {
+      id: "67159705",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586941742/lightning_ygo_api/card_images/big/67159705.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586941743/lightning_ygo_api/card_images/small/67159705.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586941744/lightning_ygo_api/card_images/art/67159705.jpg"
+    }
+  },
+  {
+    cardType: "trap",
+    name: "Attack Reflector Unit",
+    attribute: null,
+    level: null,
+    monsterType: null,
+    types: ["normal"],
+    text:
+      'Tribute 1 "Cyber Dragon". Special Summon 1 "Cyber Barrier Dragon" from your hand or Deck.',
+    atk: null,
+    def: null,
+    image: {
+      id: "91989718",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584276954/lightning_ygo_api/card_images/big/91989718.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584276956/lightning_ygo_api/card_images/small/91989718.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584276957/lightning_ygo_api/card_images/art/91989718.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Chimeratech Overdragon",
+    attribute: "dark",
+    level: 9,
+    monsterType: "machine",
+    types: ["fusion", "effect"],
+    text:
+      '"Cyber Dragon" + 1+ Machine monsters\r\nMust be Fusion Summoned. If this card is Fusion Summoned: Send all other cards you control to the GY. The original ATK/DEF of this card each become equal to the number of Fusion Materials used for its Fusion Summon x 800. Each turn, this card can attack your opponent\'s monsters a number of times equal to the number of Fusion Materials used for its Fusion Summon.',
+    atk: "?",
+    def: "?",
+    image: {
+      id: "64599569",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279250/lightning_ygo_api/card_images/big/64599569.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279251/lightning_ygo_api/card_images/small/64599569.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279252/lightning_ygo_api/card_images/art/64599569.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Chimeratech Rampage Dragon",
+    attribute: "dark",
+    level: 5,
+    monsterType: "machine",
+    types: ["fusion", "effect"],
+    text:
+      '2+ "Cyber Dragon" monsters\r\nA Fusion Summon of this card can only be done with the above Fusion Materials. When this card is Fusion Summoned: You can target Spells/Traps on the field, up to the number of Fusion Materials used for its Fusion Summon; destroy them. Once per turn: You can send up to 2 LIGHT Machine monsters from your Deck to the GY, and if you do, for each monster sent to the GY, this card gains 1 additional attack during each Battle Phase this turn.',
+    atk: 2100,
+    def: 1600,
+    image: {
+      id: "84058253",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942045/lightning_ygo_api/card_images/big/84058253.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942046/lightning_ygo_api/card_images/small/84058253.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942047/lightning_ygo_api/card_images/art/84058253.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Cyber Dragon",
+    attribute: "light",
+    level: 5,
+    monsterType: "machine",
+    types: ["effect"],
+    text:
+      "If only your opponent controls a monster, you can Special Summon this card (from your hand).",
+    atk: 2100,
+    def: 1600,
+    image: {
+      id: "70095154",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279974/lightning_ygo_api/card_images/big/70095154.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279975/lightning_ygo_api/card_images/small/70095154.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279976/lightning_ygo_api/card_images/art/70095154.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Cyber Dragon Core",
+    attribute: "light",
+    level: 2,
+    monsterType: "machine",
+    types: ["effect"],
+    text:
+      'When this card is Normal Summoned: Add 1 "Cyber" Spell/Trap from your Deck to your hand. If only your opponent controls a monster: You can banish this card from your GY; Special Summon 1 "Cyber Dragon" monster from your Deck. You can only use 1 "Cyber Dragon Core" effect per turn, and only once that turn. This card\'s name becomes "Cyber Dragon" while on the field or in the GY.',
+    atk: 400,
+    def: 1500,
+    image: {
+      id: "23893227",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279977/lightning_ygo_api/card_images/big/23893227.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279978/lightning_ygo_api/card_images/small/23893227.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279980/lightning_ygo_api/card_images/art/23893227.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Cyber Dragon Drei",
+    attribute: "light",
+    level: 4,
+    monsterType: "machine",
+    types: ["effect"],
+    text:
+      'When this card is Normal Summoned: You can make all "Cyber Dragon" you currently control become Level 5. You cannot Special Summon any monsters during the turn you activate this effect, except Machine monsters. If this card is banished: You can target 1 "Cyber Dragon" you control; it cannot be destroyed by battle or card effects this turn. This card\'s name becomes "Cyber Dragon" while on the field or in the GY.',
+    atk: 1800,
+    def: 800,
+    image: {
+      id: "59281922",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279981/lightning_ygo_api/card_images/big/59281922.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279982/lightning_ygo_api/card_images/small/59281922.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279983/lightning_ygo_api/card_images/art/59281922.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Cyber Dragon Vier",
+    attribute: "light",
+    level: 4,
+    monsterType: "machine",
+    types: ["effect"],
+    text:
+      'This card\'s name becomes "Cyber Dragon" while on the field or in the GY. If you Normal or Special Summon "Cyber Dragon", except during the Damage Step: You can Special Summon this card from your hand in Defense Position. You can only use this effect of "Cyber Dragon Vier" once per turn. Each "Cyber Dragon" you control gains 500 ATK/DEF.',
+    atk: 1100,
+    def: 1600,
+    image: {
+      id: "29975188",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942142/lightning_ygo_api/card_images/big/29975188.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942143/lightning_ygo_api/card_images/small/29975188.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942144/lightning_ygo_api/card_images/art/29975188.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Cyber Dragon Zwei",
+    attribute: "light",
+    level: 4,
+    monsterType: "machine",
+    types: ["effect"],
+    text:
+      'If this card attacks an opponent\'s monster, it gains 300 ATK during the Damage Step only. Once per turn: You can reveal 1 Spell in your hand; this card\'s name becomes "Cyber Dragon" until the End Phase. This card\'s name becomes "Cyber Dragon" while in the GY.',
+    atk: 1500,
+    def: 1000,
+    image: {
+      id: "5373478",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279984/lightning_ygo_api/card_images/big/5373478.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279986/lightning_ygo_api/card_images/small/5373478.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279988/lightning_ygo_api/card_images/art/5373478.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Cyber End Dragon",
+    attribute: "light",
+    level: 10,
+    monsterType: "machine",
+    types: ["fusion", "effect"],
+    text:
+      '"Cyber Dragon" + "Cyber Dragon" + "Cyber Dragon"\r\nA Fusion Summon of this card can only be done with the above Fusion Materials. If this card attacks a Defense Position monster, inflict piercing battle damage.',
+    atk: 4000,
+    def: 2800,
+    image: {
+      id: "1546123",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279989/lightning_ygo_api/card_images/big/1546123.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279991/lightning_ygo_api/card_images/small/1546123.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584279992/lightning_ygo_api/card_images/art/1546123.jpg"
+    }
+  },
+  {
+    cardType: "trap",
+    name: "Cyber Network",
+    attribute: null,
+    level: null,
+    monsterType: null,
+    types: ["continuous"],
+    text:
+      'Destroy this card during your 3rd Standby Phase after activation. Once per turn, if "Cyber Dragon" is on the field: You can banish 1 LIGHT Machine monster from your Deck. When this card is sent from the field to the GY: Special Summon as many of your banished LIGHT Machine monsters as possible, and if you do, destroy all Spells and Traps you control. Monsters that were Special Summoned by this effect cannot activate their effects. You cannot conduct your Battle Phase the turn you activate this effect.',
+    atk: null,
+    def: null,
+    image: {
+      id: "12670770",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280019/lightning_ygo_api/card_images/big/12670770.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280022/lightning_ygo_api/card_images/small/12670770.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280023/lightning_ygo_api/card_images/art/12670770.jpg"
+    }
+  },
+  {
+    cardType: "spell",
+    name: "Cyber Repair Plant",
+    attribute: null,
+    level: null,
+    monsterType: null,
+    types: ["normal"],
+    text:
+      'If "Cyber Dragon" is in your GY: You can activate 1 of these effects. If you have 3 or more "Cyber Dragon" in your GY at activation, you can activate both effects and resolve in sequence. You can only activate 1 "Cyber Repair Plant" per turn.\r\n● Add 1 LIGHT Machine monster from your Deck to your hand.\r\n● Target 1 LIGHT Machine monster in your GY; shuffle that target into your Deck.',
+    atk: null,
+    def: null,
+    image: {
+      id: "86686671",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280049/lightning_ygo_api/card_images/big/86686671.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280051/lightning_ygo_api/card_images/small/86686671.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280052/lightning_ygo_api/card_images/art/86686671.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Cyber Twin Dragon",
+    attribute: "light",
+    level: 8,
+    monsterType: "machine",
+    types: ["fusion", "effect"],
+    text:
+      '"Cyber Dragon" + "Cyber Dragon"\r\nA Fusion Summon of this card can only be done with the above Fusion Materials. This card can make a second attack during each Battle Phase.',
+    atk: 2800,
+    def: 2100,
+    image: {
+      id: "74157028",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280084/lightning_ygo_api/card_images/big/74157028.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280085/lightning_ygo_api/card_images/small/74157028.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584280086/lightning_ygo_api/card_images/art/74157028.jpg"
+    }
+  },
+  {
+    cardType: "spell",
+    name: "Cyberload Fusion",
+    attribute: null,
+    level: null,
+    monsterType: null,
+    types: ["quick-play"],
+    text:
+      'Fusion Summon 1 Fusion Monster from your Extra Deck that lists a "Cyber Dragon" monster as material, by shuffling the Fusion Materials listed on it into the Deck, from among your cards on the field and/or your face-up banished cards, but monsters you control cannot attack for the rest of this turn, except that Fusion Summoned monster. You can only activate 1 "Cyberload Fusion" per turn.',
+    atk: null,
+    def: null,
+    image: {
+      id: "55704856",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942149/lightning_ygo_api/card_images/big/55704856.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942150/lightning_ygo_api/card_images/small/55704856.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942151/lightning_ygo_api/card_images/art/55704856.jpg"
+    }
+  },
+  {
+    cardType: "trap",
+    name: "Cybernetic Overflow",
+    attribute: null,
+    level: null,
+    monsterType: null,
+    types: ["normal"],
+    text:
+      'Banish "Cyber Dragon(s)" with different Levels from your hand, face-up field, and/or GY, then destroy an equal number of cards your opponent controls. If this card on the field is destroyed by card effect: Add 1 "Cyber" Spell/Trap from your Deck to your hand. You can only use each effect of "Cybernetic Overflow" once per turn.',
+    atk: null,
+    def: null,
+    image: {
+      id: "82428674",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942152/lightning_ygo_api/card_images/big/82428674.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942153/lightning_ygo_api/card_images/small/82428674.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1586942154/lightning_ygo_api/card_images/art/82428674.jpg"
+    }
+  },
+  {
+    cardType: "spell",
+    name: "Evolution Burst",
+    attribute: null,
+    level: null,
+    monsterType: null,
+    types: ["normal"],
+    text:
+      'If you control "Cyber Dragon": Target 1 card your opponent controls; destroy that target. "Cyber Dragon" cannot attack the turn you activate this card.',
+    atk: null,
+    def: null,
+    image: {
+      id: "52875873",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584282663/lightning_ygo_api/card_images/big/52875873.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584282664/lightning_ygo_api/card_images/small/52875873.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584282665/lightning_ygo_api/card_images/art/52875873.jpg"
+    }
+  },
+  {
+    cardType: "spell",
+    name: "Photon Generator Unit",
+    attribute: null,
+    level: null,
+    monsterType: null,
+    types: ["quick-play"],
+    text:
+      'Tribute 2 "Cyber Dragons". Special Summon 1 "Cyber Laser Dragon" from your hand, Deck, or Graveyard.',
+    atk: null,
+    def: null,
+    image: {
+      id: "66607691",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584290466/lightning_ygo_api/card_images/big/66607691.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584290468/lightning_ygo_api/card_images/small/66607691.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584290469/lightning_ygo_api/card_images/art/66607691.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Proto-Cyber Dragon",
+    attribute: "light",
+    level: 3,
+    monsterType: "machine",
+    types: ["effect"],
+    text: 'This card\'s name becomes "Cyber Dragon" while on the field.',
+    atk: 1100,
+    def: 600,
+    image: {
+      id: "26439287",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584290782/lightning_ygo_api/card_images/big/26439287.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584290784/lightning_ygo_api/card_images/small/26439287.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584290785/lightning_ygo_api/card_images/art/26439287.jpg"
+    }
+  },
+  {
+    cardType: "monster",
+    name: "Toon Cyber Dragon",
+    attribute: "light",
+    level: 5,
+    monsterType: "machine",
+    types: ["toon", "effect"],
+    text:
+      'If your opponent controls a monster and you control no monsters, you can Special Summon this card (from your hand). Cannot attack the turn it is Summoned. While you control "Toon World" and your opponent controls no Toon monsters, this card can attack your opponent directly.',
+    atk: 2100,
+    def: 1600,
+    image: {
+      id: "83629030",
+      big:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584295042/lightning_ygo_api/card_images/big/83629030.jpg",
+      small:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584295043/lightning_ygo_api/card_images/small/83629030.jpg",
+      art:
+        "https://res.cloudinary.com/georgeletsos/image/upload/v1584295044/lightning_ygo_api/card_images/art/83629030.jpg"
+    }
+  }
+];
+
+export default {
+  mockCards
+};
