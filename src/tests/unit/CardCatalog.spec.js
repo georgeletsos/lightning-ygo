847603f0682cc744900d25fa9f2cd2154a74a2fa
georgeletsos <georgeletsos391@gmail.com>
2020-05-30 23:25:50 +0300

    CardCatalog tests refactor

--- src/tests/unit/CardCatalog.spec.js
+++ src/tests/unit/CardCatalog.spec.js
@@ -1,7 +1,8 @@
 import { mount, createLocalVue } from "@vue/test-utils";
 import CardCatalog from "@/components/CardCatalog.vue";
+import flushPromises from "flush-promises";
 import Vuex from "vuex";
-import { CHANGE_DISPLAY_CARD } from "@/store/actions.type";
+import { FETCH_DISPLAY_CARDS, CHANGE_DISPLAY_CARD } from "@/store/actions.type";
 import { mockCards } from "./mocks";
 
 const localVue = createLocalVue();
@@ -10,7 +11,8 @@ localVue.use(Vuex);
 localVue.directive("lazy", () => {});
 
 const actions = {
-  [CHANGE_DISPLAY_CARD]: () => {}
+  [CHANGE_DISPLAY_CARD]: jest.fn(),
+  [FETCH_DISPLAY_CARDS]: () => {}
 };
 const getters = {
   displayCards: () => mockCards
@@ -20,25 +22,35 @@ const store = new Vuex.Store({
   getters
 });
 
-describe("CardCatalog.vue", () => {
+describe("`CardCatalog.vue`", () => {
   it("should render correctly", () => {
     const wrapper = mount(CardCatalog, { store, localVue });
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  it("should call the |changeDisplayCard| method with a Card as payload, when this specific Card is clicked", async () => {
-    const wrapper = mount(CardCatalog, {
-      store,
-      localVue
+  it("`$store.FETCH_DISPLAY_CARDS` action subscription - should reset the catalog's scroll back to the top", async () => {
+    const wrapper = mount(CardCatalog, { store, localVue });
+    await flushPromises();
+
+    const ref = wrapper.vm.$refs.catalog;
+    ref.scrollTop = 1000;
+    store.dispatch(FETCH_DISPLAY_CARDS, {});
+    expect(ref.scrollTop).toBe(0);
+  });
+
+  describe("`methods`", () => {
+    it("`changeDisplayCard(displayCard)` - should dispatch `$store.CHANGE_DISPLAY_CARD` action", async () => {
+      const wrapper = mount(CardCatalog, {
+        store,
+        localVue
+      });
+
+      const cardIndex = 2;
+      wrapper.vm.changeDisplayCard(mockCards[cardIndex]);
+      expect(actions[CHANGE_DISPLAY_CARD]).toHaveBeenCalledTimes(1);
+      expect(actions[CHANGE_DISPLAY_CARD].mock.calls[0][1]).toEqual(
+        mockCards[cardIndex]
+      );
     });
-    const changeDisplayCardSpy = jest.spyOn(wrapper.vm, "changeDisplayCard");
-
-    await wrapper
-      .findAll(".l-cursor-pointer")
-      .at(2)
-      .trigger("click");
-
-    expect(changeDisplayCardSpy).toHaveBeenCalledTimes(1);
-    expect(changeDisplayCardSpy).toHaveBeenCalledWith(mockCards[2]);
   });
 });
