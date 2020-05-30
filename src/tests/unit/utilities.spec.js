db6a89bb841097e2a14cc5f46ac2302b5716473b
George Letsos <it1017@uom.edu.gr>
2020-05-30 21:02:39 +0300

    Utilities tests refactor

--- src/tests/unit/utilities.spec.js
+++ src/tests/unit/utilities.spec.js
@@ -7,33 +7,33 @@ import {
   isStNormal
 } from "@/common/utilities";
 
-test("|cloneObject| should return the clone of an object", () => {
+test("`cloneObject(obj)` - should return the clone of an object", () => {
   const obj = [{ a: 1, b: 2 }, { c: 3 }];
   const clonedObj = cloneObject(obj);
   expect(clonedObj).toEqual(obj);
 });
 
-test("|isMonsterCard| should return whether a card is a monster or not", () => {
+test("`isMonsterCard(card)` - should return whether a card is a monster or not", () => {
   const card = { cardType: "monster" };
   expect(isMonsterCard(card)).toBe(true);
 });
 
-test("|isTunerMonsterCard| should return whether a card is a tuner monster or not", () => {
+test("`isTunerMonsterCard(card)` - should return whether a card is a tuner monster or not", () => {
   const card = { types: ["tuner"] };
   expect(isTunerMonsterCard(card)).toBe(true);
 });
 
-test("|isSpellCard| should return whether a card is a spell or not", () => {
+test("`isSpellCard(card)` - should return whether a card is a spell or not", () => {
   const card = { cardType: "spell" };
   expect(isSpellCard(card)).toBe(true);
 });
 
-test("|isTrapCard| should return whether a card is a trap or not", () => {
+test("`isTrapCard(card)` - should return whether a card is a trap or not", () => {
   const card = { cardType: "trap" };
   expect(isTrapCard(card)).toBe(true);
 });
 
-test("|isStNormal| should return whether a S/T card is normal or not", () => {
+test("`isStNormal(card)` - should return whether a S/T card is normal or not", () => {
   const card = { types: ["normal"] };
   expect(isStNormal(card)).toBe(true);
 });
